80a20634b10807b7df0ac49f9f51a5046c2e0c47
Deguang <better_li@live.com>
2018-07-08 23:31:24 +0800

    three sum

--- Algorithms/JavaScript/015 3Sum/3Sum.js
+++ Algorithms/JavaScript/015 3Sum/3Sum.js
@@ -3,5 +3,33 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    
+    let _nums = nums.sort(),
+        len = _nums.length,
+        result = [];
+
+    for(let i = 0; i < len; i++) {
+        let target = 0 - _nums[i];
+        for(let j = i + 1; j < len; j++) {
+            for(let k = j + 1; k < len; k++) {
+                if(target === (_nums[j] + _nums[k])) {
+                    result.push([_nums[i], _nums[j], _nums[k]])
+                }
+            }
+        }
+    }
+
+    console.log(result)
+
+    let hash = {},
+        _result = [];
+    for(let l = 0, llen = result.length; l < llen; l ++) {
+        if(!hash[result[l]]) {
+            _result.push(result[l]);
+            hash[result[l]] = true;
+        }
+    }
+    console.log(_result)
+    return _result;
 };
+
+threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]);
--- Algorithms/JavaScript/015 3Sum/README.md
+++ Algorithms/JavaScript/015 3Sum/README.md
@@ -1,2 +1,4 @@
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
+# solution 1:
+use two-sum solution, find out the solution for a number as the target(0-number) of two-sum, if yes, return result, if not, return;
