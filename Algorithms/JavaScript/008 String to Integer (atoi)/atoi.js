1608a22c4f98f9c5d7f9991a8a0f97f9db072723
Deguang <better_li@live.com>
2018-07-09 23:57:18 +0800

    init atoi

--- Algorithms/JavaScript/008 String to Integer (atoi)/atoi.js
+++ Algorithms/JavaScript/008 String to Integer (atoi)/atoi.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
 var myAtoi = function(str) {
-    
+    var res = parseInt(str),
+        INT_MAX = 2147483647,
+        INT_MIN = -2147483648;
+    if(res > INT_MAX) {
+        return INT_MAX;
+    }
+    if(res < INT_MIN) {
+        return INT_MIN;
+    }
+    return isNaN(res) ? 0 : res;
 }
