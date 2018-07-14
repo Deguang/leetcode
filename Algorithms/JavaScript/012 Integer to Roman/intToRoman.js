8556c0fe217d1b08cb1a512b262a5af137eb8a3f
Deguang <better_li@live.com>
2018-07-14 23:36:30 +0800

    intToRoman

--- Algorithms/JavaScript/012 Integer to Roman/intToRoman.js
+++ Algorithms/JavaScript/012 Integer to Roman/intToRoman.js
@@ -3,46 +3,58 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    var arr = num.split(''),
+    var arr = (''+num).split(''),
         len = arr.length;
     var res = arr.map(function(item, i) {
-        switch(len - i + 1) {
+        switch(len - i) {
             case 4:
-                return Array.prototype.map.call({length: item}, function(item, i) {
+                return Array.apply(null ,{length: arr[i]}).map(function() {
                     return 'M'
-                }).join();
+                }).join('');
                 break;
             case 3:
-                if(item === 9) {
+                if(item == 9) {
                     return 'CM'
                 } else if(item > 4) {
-                    return 
+                    return 'D' + Array.apply(null ,{length: arr[i] - 5}).map(function() {
+                        return 'C'
+                    }).join('')
                 } else if(item == 4) {
                     return 'CD'
                 } else {
-
+                    return Array.apply(null ,{length: arr[i]}).map(function() {
+                        return 'C'
+                    }).join('')
                 }
                 break;
             case 2:
-                if(item === 9) {
+                if(item == 9) {
                     return 'XC';
                 } else if(item > 4) {
-                    return 
+                    return 'L' + Array.apply(null ,{length: arr[i] - 5}).map(function() {
+                        return 'X'
+                    }).join('')
                 } else if(item == 4) {
                     return 'XL';
                 } else {
-
+                    return Array.apply(null ,{length: arr[i]}).map(function() {
+                        return 'X'
+                    }).join('')
                 }
                 break;
             case 1:
-                if(item === 9) {
+                if(item == 9) {
                     return 'IX'
                 } else if(item > 4) {
-                    return 
+                    return 'V' + Array.apply(null ,{length: arr[i] - 5}).map(function() {
+                        return 'I'
+                    }).join('')
                 } else if(item == 4) {
                     return 'IV'
                 } else {
-
+                    return Array.apply(null ,{length: arr[i]}).map(function() {
+                        return 'I'
+                    }).join('')
                 }
                 break;
             default:
@@ -50,5 +62,7 @@ var intToRoman = function(num) {
         }
     })
 
-    return res.join();
+    return res.join('');
 };
+
+console.log(intToRoman(3))
