3079454c49c5cb615c4c1ec2ab8b323a6c223656
Deguang <better_li@live.com>
2018-07-22 15:31:06 +0800

    init leaf similar

---
+++ Algorithms/JavaScript/872 Leaf-Similar Trees/leafSimilar.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function(root1, root2) {
+    if(root1.left == root1.right == root2.left == root2.right == null) {
+        return root1.val == root2.val
+    }
+};
