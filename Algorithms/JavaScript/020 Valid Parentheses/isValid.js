262d9bbe354ad88881840c5a5db9cb6eb19f8482
Deguang <better_li@live.com>
2018-09-05 01:53:50 +0800

    solve valid parentheses

--- Algorithms/JavaScript/020 Valid Parentheses/isValid.js
+++ Algorithms/JavaScript/020 Valid Parentheses/isValid.js
@@ -3,5 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [],
+        arr = s.split(''),
+        map = {
+            '{': '}',
+            '[': ']',
+            '(': ')'
+        };
+
+    arr.forEach(item => {
+        let len = stack.length;
+        if(len && ( map[item] === stack[len - 1] || item === map[stack[len - 1]] )) {
+            stack.pop()
+        } else {
+            stack.push(item)
+        }
+    })
+    console.log(stack)
+    return !stack.length;
 };
+
+console.log(isValid('([)]'))
