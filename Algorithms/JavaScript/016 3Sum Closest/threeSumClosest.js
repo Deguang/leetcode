ac9931a03dc773577ebc00f381c995d926961739
Deguang <better_li@Live.com>
2018-09-11 23:46:54 +0800

    solution for three sum closet

--- Algorithms/JavaScript/016 3Sum Closest/threeSumClosest.js
+++ Algorithms/JavaScript/016 3Sum Closest/threeSumClosest.js
@@ -4,5 +4,30 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-    
+    nums = nums.sort(function(a, b) {
+        return a - b;
+    });
+
+    let len = nums.length,
+        res = 0,
+        min = Infinity;
+    for(var i = 0; i < len - 2; i++) {
+       var curr = nums[i],
+            m = i + 1,
+            n = len - 1;
+        while (m < n) {
+            var tem = Math.abs(nums[m] + nums[n] + curr - target);
+            if(tem < min) {
+                min = tem;
+                res = nums[m] + nums[n] + curr;
+            }
+
+            if(nums[m] + nums[n] + curr < target) {
+                m ++;
+            } else {
+                n --;
+            }
+        }
+    }
+    return res;
 };
