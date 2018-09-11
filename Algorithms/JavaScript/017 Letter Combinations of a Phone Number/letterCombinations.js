e015acc9a56287396f5379880f1c4d0469ee4854
Deguang <better_li@Live.com>
2018-09-12 02:07:22 +0800

    solution for letter combinations

--- Algorithms/JavaScript/017 Letter Combinations of a Phone Number/letterCombinations.js
+++ Algorithms/JavaScript/017 Letter Combinations of a Phone Number/letterCombinations.js
@@ -3,5 +3,35 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
+    if(!digits.length) return [];
+    const digitMap = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p','q','r','s'],
+        8: ['t', 'u', 'v'],
+        9: ['w','x','y','z']
+    }
     
+
+    const arr = digits.split('');
+    let resLen = 1;
+    arr.forEach(item => {
+        resLen *= digitMap[item].length;
+    })
+    let res = Array.apply(null, Array(resLen)).map(() => '');
+    let time = resLen;
+
+    arr.forEach(item => {
+        time /= digitMap[item].length;
+        res = res.map((_item, _index) => {
+            return _item + digitMap[item][Math.ceil((_index + 1)/time - 1) % digitMap[item].length]
+        })
+    })
+    console.log(res)
+    return res
 };
+
+letterCombinations('234')
